cecb298cb0c50deb68a2d769c92267bf3fd0f1dc
Shivam Goel <shvmgyl15@hotmail.com>
2021-11-09 17:23:23 +0530

    add: spring boot parent

---
+++ src/main/java/com/example/learnspring/App.java
@@ -0,0 +1,12 @@
+package com.example.learnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("Hello World!");
+    }
+}
